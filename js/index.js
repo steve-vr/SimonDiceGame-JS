6299d403ec906dc6310baea5106d6e95413c5375
SteveVR <stevevaca2020@gmail.com>
2020-11-11 11:39:52 -0500

    Estructura base del juego 'simon dice'

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+      const lightblue = document.getElementById('lightblue')
+      const violet = document.getElementById('violet')
+      const orange = document.getElementById('orange')
+      const green = document.getElementById('green')
+      const btnStart = document.getElementById('btnStart')
+
+      class Game {
+        constructor() {
+          this.initialize()
+        }
+
+        initialize() {
+          btnStar.classList.add('hide')
+        }
+      }
+
+      function startGame() {
+        var game = new Game()
+      }
