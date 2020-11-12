41c429f7ace7bebdafee0cb1275248c9c52b3af1
SteveVR <stevevaca2020@gmail.com>
2020-11-12 07:52:32 -0500

    Se configuro la secuencia de colores y el brillo de cada uno al ejecutar el juego

--- js/index.js
+++ js/index.js
@@ -7,14 +7,74 @@ const btnStart = document.getElementById('btnStart')
 
 class Game {
   constructor() {
-    this.initialize()
+    this.initialize();
+    this.generateSequence();
+    this.nextLevel();
   }
 
   initialize() {
-    btnStar.classList.add('hide')
+    btnStart.classList.add('hide');
+    this.level = 5;
+    this.colors = {
+      lightblue,
+      violet,
+      orange,
+      green
+    }
   }
+
+  generateSequence() {
+    this.sequence = new Array(10)
+    .fill(0)
+    .map(
+      n => Math.floor(
+        Math.random() * 4 
+      )
+    );
+  }
+
+  nextLevel() {
+    this.iluminateSequence();
+  }
+
+  trasnformNumberInColor(number) {
+    switch (number) {
+      case 0:
+        return 'lightblue'
+      
+      case 1:
+        return 'violet'
+      
+      case 2:
+        return 'orange'
+      
+      case 3:
+        return 'green'
+    }
+  }
+
+  iluminateSequence() {
+    for (let i = 0; i < this.level; i++) {
+      const color = this.trasnformNumberInColor(this.sequence[i]); 
+      setTimeout(
+        () => this.iluminateColor(color), 1000 * i
+      );
+    }
+  }
+
+  iluminateColor(color) {
+    this.colors[color].classList.add('light');
+    setTimeout(
+      () => this.turnOffColor(color), 350
+    );
+  }
+
+  turnOffColor(color) {
+    this.colors[color].classList.remove('light');
+  }
+
 }
 
 function startGame() {
-  var game = new Game()
+  window.game = new Game();
 }
